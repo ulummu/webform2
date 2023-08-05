1dbf05c48a63685bf400c2b1e327e5c4ffdff149
mh.ulum <misbahul0ulum@gmail.com>
2023-08-05 22:35:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import CardForm from "./Components/CardForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CardForm />
+    </div>
+  );
+}
+
+export default App;
